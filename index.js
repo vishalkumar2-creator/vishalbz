53fe319c0214bb1d3e22db9c43e5829a27abcd18
vishalkumar2-creator <dameravishalkumar25@gmail.com>
2019-12-18 23:39:24 -0800

    added html

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT||3000
-
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('public'))
+app.get('/', (req, res) => res.sendFile(__dirname+'/public/html/index.html'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
