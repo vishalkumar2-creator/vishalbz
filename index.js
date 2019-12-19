64937918735a22f37ac65affb0ee4827bd75f08d
vishalkumar2-creator <dameravishalkumar25@gmail.com>
2019-12-19 03:01:07 -0800

    added api

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ const app = express()
 const port = process.env.PORT||3000
 app.use(express.static('public'))
 app.get('/', (req, res) => res.sendFile(__dirname+'/public/html/index.html'))
-
+var obj={}
+obj.name = "Being Zero";
+obj.college = "CMRCET";
+obj.regno = "111111ABC";
+app.get('/data', (req, res) => res.json(obj))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
